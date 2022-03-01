d13bdbcc43f196e99f813dfc04e0348e0ad7a30a
Kevin Murphy-Ferguson <Kevin.MurphyFerguson@us.softvision.global>
2022-02-28 23:25:52 -0500

    initial commit

---
+++ npminstall.bat
@@ -0,0 +1,17 @@
+
+cd 05
+start npm install
+cd..\09
+start npm install
+cd..\10
+start npm install
+cd..\11
+start npm install
+cd..\12
+start npm install
+cd..\13
+start npm install
+cd..\14
+start npm install
+cd..\15
+start npm install
